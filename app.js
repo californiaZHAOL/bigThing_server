e48cbe22cc6337ad5d9c334f4053b0eb32238ed9
zhaole <ztlzlztl@outlook.com>
2022-04-12 11:39:05 +0800

    init server

---
+++ app.js
@@ -0,0 +1,13 @@
+// router文件夹仅仅存放路由
+// router_handle文件夹存放路由处理逻辑
+
+// express: 快速搭建web服务器框架
+// nodemon: 修改自启动项目
+
+// npm init -y
+// npm i express@4.17.1
+// npm i nodemon 
+
+const express = require('express');
+// 创建app的实例
+const app = express();
